4e34cb5e8b6c4ca627bf26fba6d09f6bc824e51e
QuantumCoded <QuantumCoded@users.noreply.github.com>
2019-02-01 08:16:41 -0700

    Adding more memes

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ new http.Server(function(req, res) {
       fs.createReadStream('./image.jpg').pipe(res);
       break;
 
+    case '/reee.png':
+      fs.createReadStream('./reee.png').pipe(res);
+      break;
+
+    case '/cancer.mp3':
+      fs.createReadStream('./cancer.mp3').pipe(res);
+      break;
+
     default:
       fs.createReadStream('./page.html').pipe(res);
   }
