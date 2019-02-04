42045cc349862829ead8d5a453ba3059e046777c
Brendan <brendan.fields@d11.org>
2019-02-04 14:36:37 -0700

    Converting webpage to webserver

---
+++ index.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const url  = require('url');
+const fs   = require('fs');
+
+const port = process.env.PORT || 80;
+
+const routes = {
+
+};
+
+new http.Server(function(req, res) {
+  let path = url.parse(req.url).path;
+
+  if (routes[path]) {
+    fs.createReadStream(routes[path]).pipe(res);
+  } else {
+    fs.createReadStream('./html/404.html').pipe(res);
+  }
+}).listen(port);
