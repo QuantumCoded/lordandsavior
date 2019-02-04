98351a8d801a17cf6c2007d09000cf80f1164965
Brendan <brendan.fields@d11.org>
2019-02-04 12:25:50 -0700

    Adding ryan as a subdir of main site

---
+++ ryan/scripts/main.js
@@ -0,0 +1,68 @@
+//Setting up timer and interval for notification text
+let blink_interval;
+let notif = document.getElementById('notif');
+let notif_timeout = setTimeout(function() {
+  blink_interval = setInterval(function() {
+    notif.style.opacity = "1";
+    setTimeout(function() {
+      notif.style.opacity = "0";
+    }, 500)
+  }, 1000);
+}, 3000);
+
+document.onmousedown = function(event) {
+  //Clearing timer and interval for notification text on first click
+  if (notif_timeout)  clearTimeout(notif_timeout);
+  if (blink_interval) clearInterval(blink_interval);
+  
+  if (document.getElementById('notif')) {
+    document.body.removeChild(notif);
+  }
+
+  //Generating a tomeo
+  let obj = document.body.appendChild(document.createElement('div'));
+  let _skew = 1;
+  let _x = 1;
+  let _y = 1;
+  let _r = 0;
+
+  obj.classList.add('spinner');
+  obj.style.top  = `${event.clientY - 100}px`;
+  obj.style.left = `${event.clientX - 100}px`;
+
+  let img = obj.appendChild(document.createElement('img'));
+
+  img.src = "images/tomeo.png";
+
+  //Setting up bumpy physics
+  obj.onmouseleave = function() {
+    obj.onmousemove = function() {};
+  };
+
+  obj.onmouseover = function() {
+    obj.onmousemove = function(event) {
+      console.log(event.movementX, event.movementY);
+      obj.style.top  = `${obj.offsetTop  + event.movementY}px`;
+      obj.style.left = `${obj.offsetLeft + event.movementX}px`;
+    };
+  };
+
+  //Forwarding the onmousedown event to document
+  obj.onmousedown = function(event) {
+    document.onmousedown(event);
+  };
+
+  //Setting up movement/collision detection loop
+  setInterval(function() {
+    if (obj.offsetTop <= 0) _y = 1;
+    if (obj.offsetTop + obj.offsetHeight >= window.innerHeight) _y = -1;
+    if (obj.offsetLeft <= 0) _x = 1;
+    if (obj.offsetLeft + obj.offsetWidth >= window.innerWidth) _x = -1;
+
+    obj.style.top  = `${obj.offsetTop  + _y * _skew}px`;
+    obj.style.left = `${obj.offsetLeft + _x * _skew}px`;
+
+    obj.style.transform = `rotate(${_r}deg)`;
+    _r += 1;
+  }, 2);
+};
