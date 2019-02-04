53cc79acdc9f8c62a74958a55893305b987eadd2
Brendan <brendan.fields@d11.org>
2019-02-04 14:41:53 -0700

    Adding routes

--- index.js
+++ index.js
@@ -5,7 +5,17 @@ const fs   = require('fs');
 const port = process.env.PORT || 80;
 
 const routes = {
-
+  // ./html/
+  '/'                     : './html/index.html',
+  // ./images/
+  '/images/background.jpg': './images/background.jpg',
+  '/images/tomeo.png'     : './images/tomeo.png',
+  // ./scripts/
+  '/scripts/main.js'      : './scripts/main.js',
+  // ./sounds/
+  '/sounds/background.mp3': './sounds/background.mp3',
+  // ./styles/
+  '/styles/default.css'   : './styles/default.css'
 };
 
 new http.Server(function(req, res) {
