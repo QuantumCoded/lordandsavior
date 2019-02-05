6756bfd1c90549dd570999808a97ac9f382da4cf
Fields <Brendan.Fields@d11.org>
2019-02-05 11:40:34 -0700

    Adding spacing between routes

--- index.js
+++ index.js
@@ -7,13 +7,17 @@ const port = process.env.PORT || 80;
 const routes = {
   // ./html/
   '/'                     : './html/index.html',
+
   // ./images/
   '/images/background.jpg': './images/background.jpg',
   '/images/tomeo.png'     : './images/tomeo.png',
+
   // ./scripts/
   '/scripts/main.js'      : './scripts/main.js',
+
   // ./sounds/
   '/sounds/background.mp3': './sounds/background.mp3',
+  
   // ./styles/
   '/styles/default.css'   : './styles/default.css'
 };
