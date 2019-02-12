5e97690c23b271bcc5853c46bd8316322ed00cda
Unknown <brendan.fields@d11.org>
2019-02-11 18:39:04 -0700

    Adding confirmation for cookies

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,17 @@
+//Cookie Check
 var image;
 
 try {
   image = JSON.parse(document.cookie);
-} catch(error) {console.warn(error)}
+} catch(error) {
+  //The client is not using cookies, prompt them
+  console.warn('Possible parsing failure, reseting cookies');
+  if (!confirm('This site uses cookies to save your progress, is this okay?'))
+    location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+    //Rick Roll all the scrubs that don't agree
+}
 
+//The main data object, what's stored in cookies
 var data = image ||
 {
   cash: 0,
@@ -12,18 +20,24 @@ var data = image ||
 };
 
 window.onload = function() {
-  let counter = document.getElementById('counter');
-  let button = document.getElementById('button');
+  let counter = document.getElementById('counter'); //The counter above the button
+  let button = document.getElementById('button');   //The button
 
+  //Refresh the number displayed on the counter
   var updateCash = function() {
     counter.innerHTML = `Cash: ${Math.floor(data.cash)}`;
   };  
 
+  //When the button is clicked
   button.onclick = function() {
     updateCash(data.cash += data.clickValue);
   };
 
+  //Main loop
   setInterval(function() {
+    //Wait for the user to accept cookies before saving any
+    if (!typeof data) return;
+
     updateCash(data.cash);
     document.cookie = JSON.stringify(data);
   }, 100);
